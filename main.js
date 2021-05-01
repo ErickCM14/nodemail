70b4b89f826b5cd5291505e24c4545c4295ed0a9
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-05-01 13:29:40 -0500

    Probando send-email

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var nodemailer = require("nodemailer");
+const {google} = require('googleapis')
+const { OAuth2 } = google.auth ;
 var express = require("express");
 var bodyParser = require('body-parser')
 var cors = require("cors");
@@ -11,6 +13,9 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
+let mailOptions;
+let info;
+
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -19,20 +24,34 @@ app.use(bodyParser.json())
 //     next();
 // });
 
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    post: 587,
-    secure: false,
-    auth: {
-        user: "erickcarranzameza@gmail.com",
-        pass: "Carranza140299",
-    },
-});
+const { MAILING_SERVICE_CLIENT_ID, MAILING_SERVICE_CLIENT_SECRET, 
+    MAILING_SERVICE_REFRESH_TOKEN, SENDER_EMAIL_ADDRESS} = process.env;
+
+const Mailing = {};
+
+const oauth2Client = new OAuth2(
+    MAILING_SERVICE_CLIENT_ID, MAILING_SERVICE_CLIENT_SECRET, OAUTH_PLAYGROUND
+);
+
+
+// let transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     post: 587,
+//     secure: false,
+//     auth: {
+//         user: "erickcarranzameza@gmail.com",
+//         pass: "Carranza140299",
+//     },
+// });
+
 
 app.listen(port, () => {
     console.log("Servidor en => http://localhost:3000");
 })
+
+
 
 
 //EJEMPLOS
@@ -46,19 +65,39 @@ app.post('/prueba', function (req, res, next) {
 
 app.post('/pruebaMensaje', function (req, res, next) {
     let mensaje =  req.body
+
+    let mailOptions = {
+        from: 'erickcarranzameza@gmail.com', // sender address
+        to: "erickparacompartir@gmail.com", // list of receivers
+        subject: "Vencimiento de cuentas",//"Hello ✔", // Subject line
+        text: mensaje, // plain text body
+        html: mensaje
+    };
+
+    let info = smtpTransport.sendMail(mailOptions);
+
+    // Mailing.sendEmail();
+    // callback(info);
+
+
     res.send(mensaje);
 })
-//----------
-
+//----------Termina lo de practica
+//Este es otro de practica pero se quedara como palntiila get principal
  app.get("/", (req, res) => {
     res.send("<h1 style= 'height:100vh; display:flex; justify-content:center; align-items:center; font-size:100px; '>Mercadology</h1>");
     console.log("Hola Mercadology");
 })
 
+
+
+
+
 //Envia archvio json de backup
 app.post("/send-backup", (req, res) => {
 
     let archivo = req.body;
+    req.q
 
     sendBackup(archivo, info => {
         console.log("Se envio mensaje backup");
@@ -71,7 +110,7 @@ async function sendBackup(archivo, callback) {
     let body = JSON.stringify(archivo);
     
     let mailOptions = {
-        from: '"Mercadology" <erick@mercadology.mx>', // sender address
+        from: 'erickcarranzameza@gmail.com', // sender address
         to: "erickparacompartir@gmail.com", // list of receivers
         subject: "Backup",//"Hello ✔", // Subject line
         text: "", // plain text body
@@ -79,10 +118,16 @@ async function sendBackup(archivo, callback) {
         attachments: [{
             filename: "backup.json",
             content: body
-        }]
+        }],
+        auth:
+        {
+            user: 'erickcarranzameza@gmail.com'
+        }
     };
 
-    let info = await transporter.sendMail(mailOptions);
+    let info = await smtpTransport.sendMail(mailOptions);
+    // info = await transporter.sendMail(mailOptions);
+    // Mailing.sendEmail(archivo);
 
     callback(info);
 }
@@ -93,10 +138,12 @@ async function sendBackup(archivo, callback) {
 
 //Envia correo con cuentas  a punto de vencer
 app.post("/send-email", (req, res) => {
-res.send("Hola");
+// res.send("Hola");
     let user = req.body;
+    console.log(user);
+    console.log(user.msj);
 
-    let mensaje = "<p style='font-size:15px'><strong>Cuentas que sus servicios estan por vencer:</strong></p>"
+    let mensaje = "<p style='font-size:15px'><strong>Cuentas que sus servicios estan por vencer:</strong></p>";
     user.forEach(element => {
         mensaje += "<p style='font-size:11px'><strong>" + element.msj + "</strong>" +
             "<br>Cliente: " + element.nomcli + "<br>" +
@@ -104,6 +151,7 @@ res.send("Hola");
             "Vencimiento Hosting: " + element.hosven + "<br>" +
             "Vencimiento SSL: " + element.venssl + "</p>";
     });
+    console.log(mensaje);
 
     sendMail(mensaje, info => {
         console.log("Ha sido enviado el correo");
@@ -111,18 +159,66 @@ res.send("Hola");
     })
 });
 
-async function sendMail(mensaje, callback) {
+function sendMail(mensaje, callback) {
 
     let mailOptions = {
-        from: '"Mercadology" <erick@mercadology.mx>', // sender address
-        to: "erickparacompartir@gmail.com", // list of receivers
+        from: 'erickcarranzameza@gmail.com', // sender address
+        to: "erickcarranzameza@gmail.com", // list of receivers
         subject: "Vencimiento de cuentas",//"Hello ✔", // Subject line
         text: mensaje, // plain text body
-        html: mensaje
+        html: mensaje,
+        // auth:
+        // {
+
+        // }
     };
+    console.log("Mensaje antes de sendmail");
+    let info = smtpTransport.sendMail(mailOptions);
 
-    let info = await transporter.sendMail(mailOptions);
-
+    // Mailing.sendEmail();
     callback(info);
-
 }
+
+/**Enviar correo**/
+// Mailing.sendEmail = () => {
+    // oauth2Client.setCredentials({
+    //     refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
+    // });
+
+    // const accessToken = oauth2Client.getAccessToken();
+    // console.log(accessToken);
+
+    let smtpTransport = nodemailer.createTransport({
+        // service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        // service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: 'erickcarranzameza@gmail.com',
+            pass: 'Carranza140299',
+            clientId: '987910429607-i6qs3kfss7d9bg1trth4o6rtii014t08.apps.googleusercontent.com',
+            clientSecret: 'AaUPKeGCTEOUHrrHiwuHE_1H',
+            refreshToken: '1//04JpRimDe8SvVCgYIARAAGAQSNwF-L9Ir3FpsZjAp5znhRSWiqFYgsXFter3And1j9kO94wi468xW-cUwln3kbH0U7dXoDTqCJBo',
+            accessToken: 'ya29.a0AfH6SMD9kQr1kO751pmKYBPXR0ZpWl7HLa_H8YBiwO4w5ZtmTBEC1xFH5clSjIBDAZfzAEAUaXlYZA9s39Eq2aCI6fvMpigVpozhzemAV55ZgvGA-sDIrAPxkVATOuir5_EnqraGRqx3_fXfAybdcKjzlet2',
+            // expires: 1484314697598,
+            // accessToken,
+        },
+    });
+
+    smtpTransport.set('oauth2_provision_cb', (user, renew, callback)=>{
+        // let userTokens;
+        let accessToken = user;
+        console.log(accessToken, 'aceessstokin');
+        console.log(user, 'userrrr');
+        console.log(renew, 'hhhhrenew');
+        if(!accessToken){
+            return callback(new Error('No user'));
+        }else{
+            console.log('hola');
+            return callback(null, accessToken)
+        }
+    });
+
+// }
