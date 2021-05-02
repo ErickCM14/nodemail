9d5a79629a0db3da8d1dac4bf46b8c9c8408658e
Erick Carranza Meza <erickcarranzameza@gmail.com>
2021-05-02 01:23:55 -0500

    Nuevamente

--- main.js
+++ main.js
@@ -1,102 +1,241 @@
 var nodemailer = require("nodemailer");
+const { google } = require('googleapis')
+// const  OAuth2  = google.auth.OAuth2;
 var express = require("express");
 var bodyParser = require('body-parser')
 var cors = require("cors");
 
+const port = process.env.PORT || 3000;
+// const port = 3000;
 //const { response } = require("express");
 var app = express();
-const port = process.env.PORT || 3000;
 
+// app.use(cors({ origin: '*' }));
 app.use(cors());
 app.use(bodyParser.json())
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    post: 587,
-    secure: false,
-    auth: {
-        user: "erickcarranzameza@gmail.com",
-        pass: "Carranza140299",
-    },
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 app.listen(port, () => {
     console.log("Servidor en => http://localhost:3000");
 })
 
- app.get("/", (req, res) => {
+const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
+const CLIENTID = "987910429607-i6qs3kfss7d9bg1trth4o6rtii014t08.apps.googleusercontent.com";
+const CLIENTSECRET = "AaUPKeGCTEOUHrrHiwuHE_1H";
+const REFRESH_TOKEN = "1//04nQC-Eam8m6nCgYIARAAGAQSNwF-L9IreTekJeGxMfIJslASfkHD5GYYah52eaJMjiHtWVvSkJgsn4Evq2b3K9F-vwNd_Z-ypis";
+google._options.url = 'https://developers.google.com/oauthplayground';
+// const { MAILING_SERVICE_CLIENT_ID, MAILING_SERVICE_CLIENT_SECRET, 
+//     MAILING_SERVICE_REFRESH_TOKEN, SENDER_EMAIL_ADDRESS} = process.env;
+
+const oauth2Client = new google.auth.OAuth2(
+        CLIENTID, CLIENTSECRET, 'https://developers.google.com/oauthplayground'
+);
+
+// google.options({ auth: oauth2Client, url: OAUTH_PLAYGROUND });
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN});
+
+// const access_token = new Promise((resolve, reject) => {
+//     oauth2Client.getAccessToken((err, token) => {
+//       if (err) console.log(err); // Handling the errors
+//       else resolve(token);
+//     });
+//   });
+// console.log(access_token);
+
+async function sendMail(){
+    try{
+        const accessToken = await oauth2Client.getAccessToken();
+
+        let smtpTransport = nodemailer.createTransport({    
+            // host: 'smtp.gmail.com',
+            // port: 465,
+            // secure: true,
+            service: 'gmail',
+            auth:
+            {
+                type: 'OAuth2',
+                user: 'erickcarranzameza@gmail.com',
+                pass: 'Carranza140299',
+                clientId: CLIENTID,
+                clientSecret: CLIENTSECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken,
+            },
+            tls:{
+                rejectUnauthorized:false
+            }
+        
+        })
+
+        const mailOptions = {
+                    from: 'erickcarranzameza@gmail.com', // sender address
+                    to: "erickcarranzameza@gmail.com", // list of receivers
+                    subject: "Vencimiento de cuentas",//"Hello ✔", // Subject line
+                    text: "Mensaje de prueba", // plain text body
+                    html: "<h1>Mensaje de prueba</h1>",
+                };
+        const result = await smtpTransport.sendMail(mailOptions);
+        return result;
+
+    }catch(error){
+        return error
+    }
+}
+
+sendMail()
+.then((result) => console.log("Sent email...", result))
+.catch((error) => console.log(error.message, 'gikkkk'));
+
+
+// let transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     post: 587,
+//     secure: false,
+//     auth: {
+//         user: "erickcarranzameza@gmail.com",
+//         pass: "Carranza140299",
+//     },
+// });
+
+
+//EJEMPLOS
+
+app.post('/prueba', function (req, res, next) {
+    res.send("This is CORS-enabled for all origins! POST")
+})
+
+app.post('/pruebaMensaje', function (req, res, next) {
+    let mensaje = req.body;
+    res.send(mensaje);
+})
+//----------Termina lo de practica
+//Este es otro de practica pero se quedara como palntiila get principal
+
+app.get("/", (req, res) => {
     res.send("<h1 style= 'height:100vh; display:flex; justify-content:center; align-items:center; font-size:100px; '>Mercadology</h1>");
     console.log("Hola Mercadology");
 })
 
+
+
+
+
 //Envia archvio json de backup
-app.post("/send-backup", (req, res) => {
+// app.post("/send-backup", (req, res) => {
 
-    let archivo = req.body;
+//     let archivo = req.body;
+//     req.q
 
-    sendBackup(archivo, info => {
-        console.log("Se envio mensaje backup");
-        res.send(info);
-    })
+//     sendBackup(archivo, info => {
+//         console.log("Se envio mensaje backup");
+//         res.send(info);
+//     })
 
-})
+// })
 
-async function sendBackup(archivo, callback) {
-    let body = JSON.stringify(archivo);
-    
-    let mailOptions = {
-        from: '"Mercadology" <erick@mercadology.mx>', // sender address
-        to: "erickparacompartir@gmail.com", // list of receivers
-        subject: "Backup",//"Hello ✔", // Subject line
-        text: "", // plain text body
-        html: "<p style='font-size:13px'><strong>Backup quincenal</strong></p>",
-        attachments: [{
-            filename: "backup.json",
-            content: body
-        }]
-    };
+// async function sendBackup(archivo, callback) {
+//     let body = JSON.stringify(archivo);
 
-    let info = await transporter.sendMail(mailOptions);
+//     let mailOptions = {
+//         from: 'erickcarranzameza@gmail.com', // sender address
+//         to: "erickparacompartir@gmail.com", // list of receivers
+//         subject: "Backup",//"Hello ✔", // Subject line
+//         text: "", // plain text body
+//         html: "<p style='font-size:13px'><strong>Backup quincenal</strong></p>",
+//         attachments: [{
+//             filename: "backup.json",
+//             content: body
+//         }],
+//         auth:
+//         {
+//             user: 'erickcarranzameza@gmail.com'
+//         }
+//     };
 
-    callback(info);
-}
+//     let info = await smtpTransport.sendMail(mailOptions);
+//     // info = await transporter.sendMail(mailOptions);
+//     // Mailing.sendEmail(archivo);
+
+//     callback(info);
+// }
 
 
 
 
 
-//Envia correo con cuentas  a punto de vencer
-app.post("/send-email", (req, res) => {
+// //Envia correo con cuentas  a punto de vencer
+// app.post("/send-email", (req, res) => {
+//     // res.send("Hola");
+//     let user = req.body;
+//     // console.log(user);
+//     console.log(user.msj);
 
-    let user = req.body;
+//     let mensaje = "<p style='font-size:15px'><strong>Cuentas que sus servicios estan por vencer:</strong></p>";
+//     user.forEach(element => {
+//         mensaje += "<p style='font-size:11px'><strong>" + element.msj + "</strong>" +
+//             "<br>Cliente: " + element.nomcli + "<br>" +
+//             "Nombre del dominio: " + element.domcli + "<br>" +
+//             "Vencimiento Hosting: " + element.hosven + "<br>" +
+//             "Vencimiento SSL: " + element.venssl + "</p>";
+//     });
+//     console.log(mensaje);
 
-    let mensaje = "<p style='font-size:15px'><strong>Cuentas que sus servicios estan por vencer:</strong></p>"
-    user.forEach(element => {
-        mensaje += "<p style='font-size:11px'><strong>" + element.msj + "</strong>" +
-            "<br>Cliente: " + element.nomcli + "<br>" +
-            "Nombre del dominio: " + element.domcli + "<br>" +
-            "Vencimiento Hosting: " + element.hosven + "<br>" +
-            "Vencimiento SSL: " + element.venssl + "</p>";
-    });
+//     sendMail(mensaje, info => {
+//         console.log("Ha sido enviado el correo");
+//         res.send(info);
+//     })
+// });
+// let accessToken = '';
+// function sendMail(mensaje, callback) {
 
-    sendMail(mensaje, info => {
-        console.log("Ha sido enviado el correo");
-        res.send(info);
-    })
-});
+//     let mailOptions = {
+//         from: 'erickcarranzameza@gmail.com', // sender address
+//         to: "erickcarranzameza@gmail.com", // list of receivers
+//         subject: "Vencimiento de cuentas",//"Hello ✔", // Subject line
+//         text: mensaje, // plain text body
+//         html: mensaje,
+//     };
+//     console.log("Mensaje antes de sendmail");
 
-async function sendMail(mensaje, callback) {
+//     let info = sendMail(mailOptions)
+//     .then((result) => console.log("email sent...", result))
+//     .catch((error) => console.log(error.message));
+//     // let info = smtpTransport.sendMail(mailOptions);
 
-    let mailOptions = {
-        from: '"Mercadology" <erick@mercadology.mx>', // sender address
-        to: "erickparacompartir@gmail.com", // list of receivers
-        subject: "Vencimiento de cuentas",//"Hello ✔", // Subject line
-        text: mensaje, // plain text body
-        html: mensaje
-    };
+//     // Mailing.sendEmail();
+//     callback(info);
+// }
 
-    let info = await transporter.sendMail(mailOptions);
+// /**Enviar correo**/
+// // Mailing.sendEmail = () => {
+//     // oauth2Client.setCredentials({
+//     //     refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
+//     // });
 
-    callback(info);
+//     // const accessToken = oauth2Client.getAccessToken();
+//     // console.log(accessToken);
 
-}
+//     // smtpTransport.set('oauth2_provision_cb', (user, renew, callback)=>{
+//     //     // let userTokens; 
+
+//     //     let accessToken = user.accessToken;
+//     //     console.log(accessToken, 'aceessstokin');
+//     //     console.log(user, 'userrrr');
+//     //     console.log(renew, 'hhhhrenew');
+
+//     //     if(!accessToken){
+//     //         return callback(new Error('No user'));
+//     //     }else{
+//     //         console.log('hola');
+//     //         return callback(null, accessToken)
+//     //     }
+//     // });
+
+// // }
